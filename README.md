63061a5fffcb652471c851976ed54f90d139c7f1
aad61404 <asd61404@gmail.com>
2020-09-21 00:26:01 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ITmarathon2020
+
+使用方法　
+
+```
+cd react-template
+npm install
+npm start
+```
+
+JSON-server 使用
+```
+cd react-template
+npm run server
+```
---
+++ code-template/05-react-axios/App.js
@@ -0,0 +1,30 @@
+import React , {useState} from 'react';
+import axios from 'axios'
+
+const App = (props) => {
+  const [value, setValue] = useState(10)
+
+
+  axios.get('http://localhost:3001/notes').then(response => {
+    const notes = response.data
+    console.log(notes)
+  })
+
+
+  const hello = (who) => {
+    const handler = () => {
+      alert('hello', who)
+    }
+    return handler
+  }
+
+  return (
+    <div>
+      {value}
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
+    </div>
+  )
+}
+export default App;
